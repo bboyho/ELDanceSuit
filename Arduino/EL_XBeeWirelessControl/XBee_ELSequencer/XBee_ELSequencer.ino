2b9da8743da1c0ad5e54e547b6f6c3c67686259c
Bobby <ho.chan@colorado.edu>
2017-05-09 22:33:44 -0600

    Update XBee_ELSequencer.ino

--- Arduino/EL_XBeeWirelessControl/XBee_ELSequencer/XBee_ELSequencer.ino
+++ Arduino/EL_XBeeWirelessControl/XBee_ELSequencer/XBee_ELSequencer.ino
@@ -3,7 +3,7 @@
  * Ho Yun Bobby Chan @ SparkFun Electronics June 20, 2014
  * Updated by Toni Klopfenstein @ SparkFun Electronics April, 2015
  * https://github.com/sparkfun/EL_Sequencer
- * 
+ *
  * Description:
  * This is a basic test of the EL Sequencer with a wireless controller.
  * The wireless controller consists of an Arduino Uno, XBee Explorer,
@@ -13,32 +13,32 @@
  * can be used but the throughput of the Series 1 is much higher. To
  * reduce latency, I recommend using the XBee Series 1. The basic
  * configuration of the XBee module with point-to-point configuratin is
- * based on Digi's Example tutorial 
+ * based on Digi's Example tutorial
  * => https://www.digi.com/blog/xbee/basic-xbee-802-15-4-chat/.
  * Page 5 of the tutorial shows you how to broadcast with
  * point-to-multipoint configuration so that multiple EL Sequencers
  * can be controlled.
- * 
- * By pushing the button, a character is sent from a remote microcontroller. 
+ *
+ * By pushing the button, a character is sent from a remote microcontroller.
  * The corresponding EL Sequencer will receive the character and control
  * the EL component on a channel that is associated with that character.
- * 
+ *
  * EL Sequencer uses the hardware UART of the Atmega328 for communication:
  * pin 0 = Rx
  * pin 1 = Tx
- * 
- * Note: Make sure to remove the XBee Series 1 on the EL Sequencer when 
+ *
+ * Note: Make sure to remove the XBee Series 1 on the EL Sequencer when
  * uploading a new sketch file otherwise it will brick the XBee. You can
  * always use the next generation XCTU software to unbrick and recover
  * the transceiver.
- * 
+ *
  * Development environment specifics:
  * Arduino 1.6.3
- * 
- * This code is beerware; if you see me (or any other SparkFun employee) at the local, 
+ *
+ * This code is beerware; if you see me (or any other SparkFun employee) at the local,
  * and you've found our code helpful, please buy us a round!
  * Distributed as-is; no warranty is given.
- * 
+ *
  ***********************************************************************/
 
 //Declare character 'val'
@@ -47,7 +47,7 @@ char val;
 const int status_LED = 13;
 
 /*******************Setup Loop***************************/
-void setup(){
+void setup() {
   Serial.begin(9600); //Begin Serial communication for debugging
   Serial.println("EL Sequencer's XBee is Ready to Receive Characters");
 
@@ -64,44 +64,44 @@ void setup(){
   pinMode(8, OUTPUT); //Set pin mode as output for Channel G
   pinMode(9, OUTPUT); //Set pin mode as output for Channel H
 
-for(){
-  digitalWrite(status_LED,HIGH);//set Status LED on
-  delay(50);               
-  digitalWrite(status_LED, LOW); //set Status LED off
-  delay(50);               
-}
+  for(int i = 0; i < 3; i++){
+    digitalWrite(status_LED, HIGH);//set Status LED on
+    delay(50);
+    digitalWrite(status_LED, LOW); //set Status LED off
+    delay(50);
+  }
 
-  digitalWrite(2,LOW); //Channel A
-  digitalWrite(3,LOW); //Channel B
+  digitalWrite(2, LOW); //Channel A
+  digitalWrite(3, LOW); //Channel B
 
-  digitalWrite(4,LOW); //Channel C
-  digitalWrite(5,LOW); //Channel D
+  digitalWrite(4, LOW); //Channel C
+  digitalWrite(5, LOW); //Channel D
 
-  digitalWrite(6,LOW); //Channel E
-  digitalWrite(7,LOW); //Channel F 
+  digitalWrite(6, LOW); //Channel E
+  digitalWrite(7, LOW); //Channel F
 
-  digitalWrite(8,LOW); //Channel G
-  digitalWrite(9,LOW); //Channel H 
+  digitalWrite(8, LOW); //Channel G
+  digitalWrite(9, LOW); //Channel H
 
   delay(1000); //Wait 1 second
 }
 
 /*******************Main Loop***************************/
-void loop(){
+void loop() {
 
   //Check if XBee is receiving data from other XBee
-  if(Serial.available()){
+  if (Serial.available()) {
     val = Serial.read();
 
     //Check to see if character sent is letter A
-    if(val == 'A'){
-      digitalWrite(13,HIGH); //turn ON Status LED
+    if (val == 'A') {
+      digitalWrite(13, HIGH); //turn ON Status LED
       delay(50);
       Serial.println("Character Received");
     }
 
-    else{
-      digitalWrite(13,LOW); //turn OFF Status LED
+    else {
+      digitalWrite(13, LOW); //turn OFF Status LED
     }
   }
 }
